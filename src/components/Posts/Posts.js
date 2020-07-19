f23d783c457db84142a74a8c5f4f1246f2797f63
retroazure <tomascosta005@outlook.com>
2020-07-19 17:17:31 +0100

    mvp tasks

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,6 +8,7 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
+      {posts.map(item => <Post post={item} likePost={likePost}/>)}
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
